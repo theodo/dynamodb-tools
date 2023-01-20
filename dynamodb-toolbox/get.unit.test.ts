4edb52641522d379764269b9c48bfe06a464548c
ThomasAribart <thomasa@theodo.fr>
2023-01-20 17:12:35 +0100

    add tests for get & update in dynamodb-toolbox

---
+++ dynamodb-toolbox/get.unit.test.ts
@@ -0,0 +1,52 @@
+import MockDate from 'mockdate';
+
+import { PokemonInstanceEntity } from './Entity';
+import { table } from './Table';
+
+const pokemonMasterId = '123';
+const pokemonInstanceId = '456';
+const pokemonName = 'Pikachu';
+const level = 42;
+const isLegendary = false;
+const captureDate = '2021-01-01T00:00:00.000Z';
+
+const now = new Date().toISOString();
+MockDate.set(now);
+
+const pokemonInstance = {
+  pokemonInstanceId,
+  pokemonName,
+  level,
+  isLegendary,
+  pokemonMasterId,
+  captureDate,
+};
+
+describe('dynamodb-toolbox - get', () => {
+  it('should get an item', () => {
+    expect(
+      PokemonInstanceEntity.getParams({ pokemonInstanceId }),
+    ).toStrictEqual({
+      Key: {
+        PK: 'PokemonInstance',
+        SK: pokemonInstanceId,
+      },
+      TableName: table.name,
+    });
+  });
+
+  it('should add the correct projection expression (exists & capture date in past)', () => {
+    expect(
+      PokemonInstanceEntity.getParams(pokemonInstance, {
+        attributes: ['pokemonName', 'level', 'isLegendary'],
+      }),
+    ).toMatchObject({
+      ExpressionAttributeNames: {
+        '#proj1': 'pokemonName',
+        '#proj2': 'level',
+        '#proj3': 'isLegendary',
+      },
+      ProjectionExpression: '#proj1,#proj2,#proj3',
+    });
+  });
+});
